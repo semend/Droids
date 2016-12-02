855682e22d79642f404062e5e574049e81edd83a
semend <semadavis@gmail.com>
2016-12-02 15:43:52 +0300

    The app

---
+++ droids/GeoQuiz/app/src/main/java/com/automata/falc0n/geoquiz/Question.java
@@ -0,0 +1,27 @@
+package com.automata.falc0n.geoquiz;
+
+/**
+ * Created by falc0n on 11/12/16.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+    public int getTextResId() {
+        return mTextResId;
+    }
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
